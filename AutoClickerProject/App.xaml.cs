79ff3f47d76f648cce3fbf0c005a68c9c2629933
hp <hp@JIMMY>
2023-11-20 13:41:06 +0200

    Add project files.

---
+++ AutoClickerProject/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AutoClickerProject
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
